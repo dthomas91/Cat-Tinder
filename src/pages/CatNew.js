d5f1a6cc6a8d8442c9f3ab9554e9395a084b78d6
ZackRohde <zdrohde@outlook.com>
2022-10-31 12:56:17 -0700

    create functionality working, need to test still

--- src/pages/CatNew.js
+++ src/pages/CatNew.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, FormGroup, Input, Label, Button } from "reactstrap";
+import { useNavigate } from "react-router-dom";
 
-const CatNew = () => {
-    return(
-        <>CatNew</>
-    )
+const CatNew = ({ createCat }) => {
+  const navigate = useNavigate()
+  const [newCat, setNewCat] = useState({
+    name: "",
+    age: "",
+    enjoys: "",
+    image: ""
+  })
+  const handleChange = (e) => {
+    setNewCat({ ...newCat, [e.target.name]: e.target.value })
+  }
+  const handleSubmit = () => {
+    createCat(newCat)
+    navigate("/catindex")
+  }
+
+  return (
+    <>
+      <Form>
+        <FormGroup>
+          <Label for="name">Name</Label>
+          <Input type="text" name="name" placeholder="What is your name?" onChange={handleChange} value={newCat.name} />
+        </FormGroup>
+        <FormGroup>
+          <Label for="age">Age</Label>
+          <Input type="number" name="age" placeholder="How old are you?" onChange={handleChange} value={newCat.age} />
+        </FormGroup>
+        <FormGroup>
+          <Label for="enjoys">Enjoys</Label>
+          <Input type="text" name="enjoys" placeholder="What do you ennjoy?" onChange={handleChange} value={newCat.enjoys} />
+        </FormGroup>
+        <FormGroup>
+          <Label for="image">Image</Label>
+          <Input type="url" name="image" placeholder="Upload a photo of you!" onChange={handleChange} value={newCat.image} />
+        </FormGroup>
+        <Button onClick={handleSubmit} name="submit">
+          Submit Cat
+        </Button>
+      </Form>
+    </>
+  )
 }
 
 export default CatNew
