43749e6d828fe3d48fb91af6599ebdee9acd54fe
dthomas91 <davidthomas8891@gmail.com>
2022-11-01 09:56:56 -0700

    Finished fetch challenges

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import mockCats from "./mockCats";
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -11,10 +10,44 @@ import CatEdit from './pages/CatEdit';
 import NotFound from './pages/NotFound'
 
 const App = () => {
-  const [cats, setCats] = useState(mockCats)
+  const [cats, setCats] = useState([])
+
+  useEffect(() => {
+    readCat()
+  }, [])
+
+  const readCat = () => {
+    fetch("http://localhost:3000/cats")
+    .then((response) => response.json())
+    .then((payload) => {
+      setCats(payload)
+    })
+    .catch((error) => console.log(error))
+  }
 
   const createCat = (cat) => {
-    console.log(cat)
+    fetch("http://localhost:3000/cats", {
+      body: JSON.stringify(cat),
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST"
+    })
+    .then((response) => response.json())
+    .then((payload) => readCat())
+    .catch((errors) => console.log("Cat create errors:", errors))
+  }
+
+  const deleteCat = (id) => {
+    fetch(`http://localhost:3000/cats/${id}`, {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "DELETE"
+    })  
+    .then((response) => response.json())
+    .then((payload) => readCat())
+    .catch((errors) => console.log("delete errors:", errors))
   }
 
   const updateCat = (cat, id) => {
@@ -28,7 +61,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/catindex" element={<CatIndex cats={cats} />} />
-        <Route path="/catshow/:id" element={<CatShow cats={cats} />} />
+        <Route path="/catshow/:id" element={<CatShow cats={cats} deleteCat={deleteCat} />} />
         <Route path="/catnew" element={<CatNew createCat={createCat} />} />
         <Route path="/catedit/:id" element={<CatEdit cats={cats} updateCat={updateCat} />} />
         <Route path="*" element={<NotFound />} />
